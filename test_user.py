5c3810a13f80d28463e5bad457b2437825cc4eaa
hossein <hosseinmoradian112@gmail.com>
2026-09-24 08:25:00 -0700

    test

---
+++ test_user.py
@@ -0,0 +1,12 @@
+"""
+The provided file allows you to directly insert your code and start working with this library.
+Examples and Guidelines in github -->  https://github.com/hosssein193m-ai/DataForge
+"""
+from src.ascii_binary import * 
+from src.charsets import *
+from src.IRAN import *
+from src.security import *
+from src.world import *
+
+# *** Satrt ***
+
